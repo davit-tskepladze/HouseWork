8b1196d05cfd64bbdae29175276a6a39d8d08209
davit-tskepladze <dtskepladze3@gmail.com>
2022-04-28 19:08:42 +0400

    first commit

---
+++ week3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace week3
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Davit Tskepladze");
+                var input = Console.ReadLine();
+                Console.WriteLine(input);
+            
+        
+    
+
+}
+    }
+}
